ceb15d65082079d00b0585aa23d2bc0ebd508205
Yarmacho <dima.yrmak@gmail.com>
2021-04-17 14:40:35 +0300

    Commit

--- src/StackList.java
+++ src/StackList.java
@@ -1,47 +1,77 @@
-import java.util.LinkedList;
-
 public class StackList<T>  extends Stack<T>
 {
-    private LinkedList<T> _stack;
-
-    public StackList()
-    {
-        _stack = new LinkedList<T>();
-    }
+    private Node<T> _topElement;
 
     @Override
     public T Peek() {
-        if (_stack.isEmpty())
+        if (_topElement == null)
         {
             throw new IndexOutOfBoundsException("Стек пуст");
         }
 
-        return _stack.getLast();
+        return _topElement.Value;
     }
 
     @Override
     public T Pop() {
-        if (_stack.isEmpty())
+        if (_topElement == null)
         {
             throw new IndexOutOfBoundsException("Стек пуст");
         }
-
-        return _stack.pop();
+        var value = _topElement.Value;
+        _topElement = _topElement.PreviousNode;
+        return value;
     }
 
     @Override
     public void Push(T item)
     {
-        _stack.addLast(item);
+        if (_topElement == null)
+        {
+            _topElement = new Node<>(item);
+        }
+        else
+        {
+            var node = new Node<T>(item);
+            node.PreviousNode = _topElement;
+            _topElement = node;
+        }
     }
 
     @Override
     public int Count() {
-        return _stack.size();
+        int count = 0;
+        for (var tmp = _topElement; tmp != null; tmp = tmp.PreviousNode)
+        {
+            count++;
+        }
+
+        return count;
     }
 
     @Override
     protected T getByIndex(int index) {
-        return _stack.get(index);
+        var count = Count();
+        if (index < 0 || index >= count)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+
+        var indexFromLastElement = count - index - 1;
+        var node = _topElement;
+        for (int i = 0; i < indexFromLastElement; node = node.PreviousNode, i++);
+
+        return node.Value;
+    }
+
+    private class Node<T>
+    {
+        public Node PreviousNode;
+        public T Value;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
     }
 }
